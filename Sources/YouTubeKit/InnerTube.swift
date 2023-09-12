356c81e5ac4a16053aea5d830c3b6be49e122117
Cihat Gündüz <commits@cihatguenduez.de>
2023-09-12 09:56:52 +0200

    Send Web as client type to fix issues with missing audio (& more)

--- Sources/YouTubeKit/InnerTube.swift
+++ Sources/YouTubeKit/InnerTube.swift
@@ -67,7 +67,7 @@ class InnerTube {
     
     private let baseURL = "https://www.youtube.com/youtubei/v1"
     
-    init(client: ClientType = .ios, useOAuth: Bool = false, allowCache: Bool = true) {
+    init(client: ClientType = .web, useOAuth: Bool = false, allowCache: Bool = true) {
         self.context = defaultClients[client]!.context
         self.apiKey = defaultClients[client]!.apiKey
         self.headers = defaultClients[client]!.headers
