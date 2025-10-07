50c02171307e433bb3249a8a5574761e55abf58b
Alexander Eichhorn <a.eichhorn@losjet.com>
2025-10-07 11:30:35 +0200

    fix for some devices where no streams were found

--- Sources/YouTubeKit/YouTube.swift
+++ Sources/YouTubeKit/YouTube.swift
@@ -101,6 +101,7 @@ public class YouTube {
             var request = URLRequest(url: extendedWatchURL)
             request.setValue("Mozilla/5.0", forHTTPHeaderField: "User-Agent")
             request.setValue("en-US,en", forHTTPHeaderField: "accept-language")
+            request.httpShouldHandleCookies = false
             let (data, _) = try await URLSession.shared.data(for: request)
             _watchHTML = String(data: data, encoding: .utf8) ?? ""
             return _watchHTML!
@@ -115,6 +116,7 @@ public class YouTube {
             var request = URLRequest(url: embedURL)
             request.setValue("Mozilla/5.0", forHTTPHeaderField: "User-Agent")
             request.setValue("en-US,en", forHTTPHeaderField: "accept-language")
+            request.httpShouldHandleCookies = false
             let (data, _) = try await URLSession.shared.data(for: request)
             _embedHTML = String(data: data, encoding: .utf8) ?? ""
             return _embedHTML!
