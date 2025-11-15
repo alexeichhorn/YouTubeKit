713a84deb3273fd414e241b0d24fc7e2101cd1e6
alexandr potapov <alex.potapov.dev00@gmail.com>
2025-11-15 19:13:17 +0300

    add platforms .iOS(.v15)

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "YouTubeKitPlayer",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         .library(
             name: "YouTubeKitPlayer",
