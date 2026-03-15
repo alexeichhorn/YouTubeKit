66861a7afe0ff0f397621f8dc2f26bb823a92bd9
Alexander Eichhorn <a.eichhorn@losjet.com>
2026-03-15 16:58:26 +0100

    removed fixed player override

--- Tests/YouTubeKitTests/ExtractionTests.swift
+++ Tests/YouTubeKitTests/ExtractionTests.swift
@@ -17,16 +17,16 @@ final class ExtractionTests: XCTestCase {
         XCTAssertEqual(videoID, "2lAe1cqCOXo")
     }
 
-    func testJSURLUsesPinnedPlayerOverride() throws {
+    func testJSURLUsesExtractedPlayerPath() throws {
         let html = #"ytcfg.set({"STS":20515});var ytInitialPlayerResponse = {};var ytplayer = {"config":{"assets":{"js":"/s/player/6c5cb4f4/player_ias.vflset/en_US/base.js"}}};"#
         let jsURL = try Extraction.jsURL(html: html)
-        XCTAssertEqual(jsURL, "https://youtube.com/s/player/9f4cc5e4/player_ias.vflset/en_US/base.js")
+        XCTAssertEqual(jsURL, "https://youtube.com/s/player/6c5cb4f4/player_ias.vflset/en_US/base.js")
     }
 
-    func testSignatureTimestampUsesPinnedOverride() throws {
+    func testSignatureTimestampUsesExtractedValue() throws {
         let js = "signatureTimestamp:20515"
         let signatureTimestamp = Extraction.extractSignatureTimestamp(fromJS: js)
-        XCTAssertEqual(signatureTimestamp, 20514)
+        XCTAssertEqual(signatureTimestamp, 20515)
     }
 
     func testExtractYtCfgParsesEmbeddedEncryptedHostFlags() throws {
